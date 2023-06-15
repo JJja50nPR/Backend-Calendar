26e171c69e5dc45c8df8ccf5a0d5704453f14dca
Jason <jasonpalacios1346@gmail.com>
2023-06-15 17:05:39 -0600

    Redirects

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
 
-app.get('*', ( req, res ) => {
-    res.sendFile('/public/index.html');
-});
-
-
 //Escuchar peticiones
 app.listen( process.env.PORT , () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
